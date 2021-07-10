07f49bd738a9f0be98682c92a54d35de2fcbddc8
techytreads <sachinpatil@Sachins-MacBook-Pro.local>
2021-07-11 01:48:25 +0530

    pushing the first code

---
+++ src/Instructions.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+export class Instructions extends Component {
+
+  render() {
+    return(
+      <p>Hello this is first integrated component</p>
+    )
+  }
+
+}
+
+export default Instructions;
